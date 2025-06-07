c80a7f312a863b84bfee50239345ff53ab9ab59c
Sneha Das <154408198+Snehadas2005@users.noreply.github.com>
2025-06-07 21:38:09 +0530

    ntohing changed

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -1,21 +1,19 @@
 import CountUp from 'react-countup';
-import { counterItems } from "../constants"
-
+import { counterItems } from "../constants";
 
 export const AnimatedCounter = () => {
   return (
-    <div id="counter" class name="padding-X-lg xl:mt-0 mt-32">
-        <div className="max-auto grid-4-cols">
-            {counterItems.map((item) => (
-                <div className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
-                    <div key={counterItems.label} className="counter-number text-white text-5xl font-bold mb-2">
-                        <CountUp suffix={item.suffix} end={item.value}/>
-                    </div>
-                    <div className="text-white-50 text-lg">{item.label}</div>
-                </div>
-            ))}
-        </div>
-
+    <div id="counter" className="px-6 xl:mt-0 mt-32">
+      <div className="mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        {counterItems.map((item) => (
+          <div key={item.label} className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center items-center">
+            <div className="counter-number text-white text-5xl font-bold mb-2">
+              <CountUp end={item.value} suffix={item.suffix} />
+            </div>
+            <div className="text-white/70 text-lg">{item.label}</div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,9 @@
+if (process.env.NODE_ENV === "development") {
+  const script = document.createElement("script");
+  script.src = "http://localhost:8097";
+  document.head.appendChild(script);
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
